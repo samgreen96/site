a2c4a0c205660fcc35b6667a3ce42c74bbb034b2
Sam Green <sam@socialpinpoint.com>
2023-04-30 16:19:04 +1000

    Playing around with content and styling

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -4,4 +4,13 @@ import "syntax-highlighting.css"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("Bridgetown is loaded!")
+window.cvToPDF = () => {
+  var element = document.getElementById('cv');
+  var opt = {
+    margin: 5,
+    filename: 'Sam-Green-CV.pdf',
+    html2canvas: { scale: 1 },
+  }
+
+  html2pdf().set(opt).from(element).save();
+}
